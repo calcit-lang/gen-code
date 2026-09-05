c5857dcb41b554ba9369ba47177639dc288a3908
tiye <jiyinyiyong@gmail.com>
2026-09-05 21:23:08 +0800

    chore: refresh PnP resolver

--- .pnp.cjs
+++ .pnp.cjs
@@ -28,7 +28,7 @@ const RAW_RUNTIME_STATE =
       [null, {\
         "packageLocation": "./",\
         "packageDependencies": [\
-          ["@calcit/procs", "npm:0.13.75"],\
+          ["@calcit/procs", "npm:0.13.77"],\
           ["@cirru/writer.ts", "npm:0.1.7"],\
           ["@google/genai", "virtual:dc3fc578bfa5e06182a4d2be39ede0bc5b74940b1ffe0d70c26892ab140a4699787750fba175dc306292e80b4aa2c8c5f68c2a821e69b2c37e360c0dff36ff66#npm:1.4.0"],\
           ["bottom-tip", "npm:0.1.5"],\
@@ -42,10 +42,10 @@ const RAW_RUNTIME_STATE =
       }]\
     ]],\
     ["@calcit/procs", [\
-      ["npm:0.13.75", {\
-        "packageLocation": "./.yarn/cache/@calcit-procs-npm-0.13.75-82bc157339-e07fabba49.zip/node_modules/@calcit/procs/",\
+      ["npm:0.13.77", {\
+        "packageLocation": "./.yarn/cache/@calcit-procs-npm-0.13.77-cca95b1669-7f0d79ad9e.zip/node_modules/@calcit/procs/",\
         "packageDependencies": [\
-          ["@calcit/procs", "npm:0.13.75"],\
+          ["@calcit/procs", "npm:0.13.77"],\
           ["@calcit/ternary-tree", "npm:0.0.26"],\
           ["@cirru/parser.ts", "npm:0.0.9"],\
           ["@cirru/writer.ts", "npm:0.1.9"]\
@@ -1519,7 +1519,7 @@ const RAW_RUNTIME_STATE =
       ["workspace:.", {\
         "packageLocation": "./",\
         "packageDependencies": [\
-          ["@calcit/procs", "npm:0.13.75"],\
+          ["@calcit/procs", "npm:0.13.77"],\
           ["@cirru/writer.ts", "npm:0.1.7"],\
           ["@google/genai", "virtual:dc3fc578bfa5e06182a4d2be39ede0bc5b74940b1ffe0d70c26892ab140a4699787750fba175dc306292e80b4aa2c8c5f68c2a821e69b2c37e360c0dff36ff66#npm:1.4.0"],\
           ["bottom-tip", "npm:0.1.5"],\
